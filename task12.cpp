5b85a4f0e8e04740c7af074fc725c890103aaa35
Aleena-01 <aleenazafar1818@gmail.com>
2023-09-15 09:35:11 -0700

    week2ofPD

---
+++ task12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main() {
+cout<<"           *        *       *                   "<<endl;
+cout<<"             *        *       *                  "<<endl;
+cout<<"               *        *       *                 "<<endl;
+cout<<"             *        *       *                  "<<endl;
+cout<<"            *       *       *                   "<<endl;
+cout<<"             *       *       *                 "<<endl;
+cout<<"              *       *        *               "<<endl;
+cout<<"        ******************************                                      "<<endl;
+cout<<"       *                              *         "<<endl;
+cout<<"        *                            *       "<<endl;
+cout<<"         *                          *          "<<endl;
+cout<<"           *                       *           "<<endl;
+cout<<"             ********************                                 "<<endl;
+cout<<"        #welcome to foody application                                      "<<endl;                                              "<<endl;
---
+++ task8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+main() {
+cout<<"                   eeeee       eeeeee                               "<<endl;
+cout<<"                  e     e     e      e                       "<<endl;
+cout<<"                     eee      e      e                     "<<endl;
+cout<<"                   ee         e      e                      "<<endl;
+cout<<" .........        eeeeeee      eeeeee   th     ..........                                      "<<endl;
+cout<<"  ........      ---------------------------    .........                                           "<<endl;
+cout<<"   .......      C   E    N    T   U   R   Y    .......                                          "<<endl;
+cout<<"     .....      ---------------------------    .....                                        "<<endl;
+cout<<"      ....     eeeee  eeeeee  e   e  eeeeee    ....                               "<<endl;
+cout<<"       ...       e    e        e e     e       ...          "<<endl;
+cout<<"       _||_      e    eeee      e      e     _||_                "<<endl;
+cout<<"     |     |     e    e       e  e     e    |     |       "<<endl;
+cout<<"   __|_____|___  e    eeeeee e    e    e  __|_____|___                                                    "<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+}
